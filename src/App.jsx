922ccfbc8b98811084c24aabe817db9e824f7784
HamzaKhan001 <spaceus9@gmail.com>
2023-06-18 19:10:48 +0500

    app.js reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { BrowserRouter } from 'react-router-dom';
+import { Navbar, Contact, Experience, Feedbacks, Hero, Tech , Works, StarsCanvas, About} from './components'
 
 const App = () => {
 
   return (
   <>
+  <BrowserRouter>
+  <div className='relative z-0 bg-primary'>
+    <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <Navbar />
+      <Hero />
+    </div>
+    <About />
+    <Experience />
+    <Tech />
+    <Works />
+    <Feedbacks />
+    
+  </div>
+  </BrowserRouter>
   </>
   )
 }
