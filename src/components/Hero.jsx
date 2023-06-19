512124da23ba5c82f862f470da915fbef3525cf6
HamzaKhan001 <spaceus9@gmail.com>
2023-06-19 22:25:34 +0500

    3d model displayed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
   }
 
   return (
-    <section className='relative w-full h-screen mx-auto'>
+    <section className='relative w-full h-screen mx-auto '>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>  
           <div className='w-5 h-5 rounded-full bg-[#915eff]' />
@@ -29,9 +29,12 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}> Hi, I am <span className='text-[#915eff]'>Hamza Khan</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop full stack web applications <br className='sm:block hidden'/> from both GIS & non-GIS domains
+          </p>
         </div>
       </div>
-
+      <ComputersCanvas />
     </section> 
   )
 }
