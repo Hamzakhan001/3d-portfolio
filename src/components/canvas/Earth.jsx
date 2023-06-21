36f69d38efc8322c8b050efdb196d437e6952b30
HamzaKhan001 <spaceus9@gmail.com>
2023-06-22 01:57:40 +0500

    contact form

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls,Preload, useGLTF } from '@react-three/drei';
 
 const Earth = () => {
+
+  const earth = useGLTF('./planet/scene.gif')
+
   return (
-    <div>Earth</div>
+    <primitive object={earth.scene} scale={2.5} position-y ={0} rotation-y/>
   )
 }
 
@@ -12,6 +15,22 @@ const EarthCanvas = () => {
   return(
     <Canvas shadows frameloop='demand'
     gl={{preserveDrawingBuffer:true}}
+    camera={{ 
+      fov: 45,
+      near:0.1,
+      far: 200,
+      position: [-4 , 3, 6]
+
+    }} >
+
+    <Suspense>
+      <OrbitControls autoRotate enableZoom={false}
+      maxPolarAngle={Math.PI/2}
+      minPolarAngle={Math.PI /2}
+      />
+      <Earth />
+    </Suspense>
+    </Canvas>
   )
 }
 
