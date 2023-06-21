f878effe6af123f7cc4fc0228d8ea2b8b5a79973
HamzaKhan001 <spaceus9@gmail.com>
2023-06-22 01:18:01 +0500

    contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,7 +26,14 @@ const Contact = () => {
 
 
   return (
-    <div>Contact</div>
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+      <motion.div variants={slideIn('left','tween',0.2,1)}
+      className='flex-[0.75] bg-black-100 p-8 rounded-2xl'>
+        <p className={styles.sectionSubText}>Get In Touch</p>
+        
+      </motion.div>
+
+    </div>
   )
 }
 
