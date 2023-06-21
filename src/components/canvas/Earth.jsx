2ba793f958ac94bc8e5b13f5442978be07ec3af1
HamzaKhan001 <spaceus9@gmail.com>
2023-06-22 01:45:00 +0500

    contact form

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls,Preload, useGLTF } from '@react-three/drei';
 
 const Earth = () => {
   return (
     <div>Earth</div>
+  )
+}
+
+const EarthCanvas = () => {
+  return(
+    <Canvas shadows frameloop='demand'
+    gl={{preserveDrawingBuffer:true}}
   )
 }
 
