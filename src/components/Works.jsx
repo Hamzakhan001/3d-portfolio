a3ed7a376ad450ee4567a60512d294468fbf3471
HamzaKhan001 <spaceus9@gmail.com>
2023-06-21 23:15:27 +0500

    imports in works section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { styles } from '../styles';
+import { github } from '../assets';
+import { SectionWrapper } from '../hoc';
+import { projects } from '../constants';
+import { fadeIn,textVariant } from '../utils/motion';
 
 const Works = () => {
   return (
